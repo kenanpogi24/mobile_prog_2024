1f7b4eda2317dbfbe314e8f7baac602d7695df62
Khennan <2202709@ub.edu.ph>
2024-09-07 11:03:09 +0800

    Demo GIT Change

---
+++ play_demogitchange.js
@@ -0,0 +1,22 @@
+console.log("Demo GIT change");
+
+// ADDING NEW PROJECT AND USING GIT 
+/* 
+    1. Create a new repository on GitHub
+    2. Go to the folder where your project is located
+    3. Run git init  
+    4. Run git add . 
+    5. Run commit -m "Initial commit"
+    6. Run git branch -M main
+    7. Run git remote add origin <url>
+    8. Run git push-u origin main 
+*/
+
+
+// ADD/EDIT, COMMIT AND PUSH 
+/*
+    1. git add . 
+    2. git commit -m "Demo GIT change"
+    3. git push 
+
+*/
